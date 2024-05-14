d252d87a31c181cec0b48b8526409588c0f4a323
chase-pham <162328590+chase-pham@users.noreply.github.com>
2024-05-13 19:34:32 -0500

    Sort function, SQL update

--- src/SQLQuery1.sql
+++ src/SQLQuery1.sql
@@ -10,4 +10,12 @@ CREATE TABLE Tasks (
     Status BIT
 );
 GO
+-- Alter the 'Tasks' table to add enew columns if it already exists
+ALTER TABLE Tasks
+ADD Priority INT DEFAULT 1, -- Adding priority column with default value 1
+	CompletionStatus BIT DEFAULT 0; -- Adding CompletionStatus column with default value 0 (incomplete)
+GO
+
+-- Grant permissions to 'TestUser'
 GRANT INSERT, SELECT, UPDATE, DELETE ON dbo.Tasks TO TestUser;
+GO
