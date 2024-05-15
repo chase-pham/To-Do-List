6fc0858937ff09f64c1acad6b378aa348b09aacf
chase-pham <162328590+chase-pham@users.noreply.github.com>
2024-05-14 19:19:59 -0500

    SQL delete Description, Status column

--- src/SQLQuery1.sql
+++ src/SQLQuery1.sql
@@ -5,9 +5,7 @@ IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Tasks')
 CREATE TABLE Tasks (
     TaskID INT PRIMARY KEY IDENTITY(1,1),
     TaskNAME VARCHAR(255) NOT NULL,
-    Description TEXT,
     DueDate DATE,
-    Status BIT
 );
 GO
 -- Alter the 'Tasks' table to add enew columns if it already exists
