6d157bae37fa3a9619d556a8b9265d595a380826
chase-pham <162328590+chase-pham@users.noreply.github.com>
2024-05-20 18:06:56 -0500

    TaskController update

--- src/main/java/com/example/demo/TaskController.java
+++ src/main/java/com/example/demo/TaskController.java
@@ -2,17 +2,19 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
     @Autowired
     private TaskRepository taskRepository;
 
-    @GetMapping("/tasks")
+    @GetMapping
     public List<Task> getTasks() {
         return taskRepository.findAll();
     }
